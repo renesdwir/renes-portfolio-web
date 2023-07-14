7899b4cf41d6916eef0d12309ec5115d6949d47d
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-07-14 21:11:14 +0700

    refactor title

--- src/components/tech.component.tsx
+++ src/components/tech.component.tsx
@@ -1,6 +1,7 @@
 import { TechDataInterface } from "@/data/data-types";
 import Image from "next/image";
 import Link from "next/link";
+import Title from "./title.component";
 interface TechPropsTypes {
   data: TechDataInterface[];
 }
@@ -8,10 +9,7 @@ const Tech = (props: TechPropsTypes) => {
   const { data: datas } = props;
   return (
     <div id="skills" className="pt-16 mb-16 lg:pt-0 lg:mb-36">
-      <div className="font-bold  uppercase sticky top-0 z-20 -mx-6 mb-4 bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12  lg:relative lg:top-auto lg:mx-auto  lg:px-0 lg:py-0 lg:bg-transparent  ">
-        Some Technologies I've worked with
-        <span className="text-cstmgreen ml-1">:</span>
-      </div>
+      <Title text="Some Technologies I've worked with" />
       <div className="flex flex-row gap-4 flex-wrap">
         {datas.map(({ icon, name, url }, idx) => (
           <Link href={url} rel="noopener noreferrer" target="_blank" key={idx}>
