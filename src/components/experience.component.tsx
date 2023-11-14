41f8397ce604af20aaf618386968cecc97d74020
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-11-14 20:56:43 +0700

    refactor experience

--- src/components/experience.component.tsx
+++ src/components/experience.component.tsx
@@ -1,39 +1,36 @@
-import { ExperienceDataInterface } from "@/data/data-types";
 import Title from "./title.component";
 import Link from "next/link";
 import TechIcon from "./techIcon.component";
-interface ExperiencePropsTypes {
-  data: ExperienceDataInterface[];
-}
-const Experience = (props: ExperiencePropsTypes) => {
-  const { data: datas } = props;
+import { experience_data } from "@/data/data";
+
+const Experience = () => {
+  const datas = experience_data;
   return (
     <div id="experience" className="pt-16 mb-16 lg:pt-0 lg:mb-36 ">
       <Title text="Experience" />
-      <div className="flex flex-col gap-8">
+      <div className="flex flex-col ">
         {datas.map(({ at, date, description, link, position, tech }, idx) => (
-          <Link
-            href={link}
-            rel="noopener noreferrer"
-            target="_blank"
-            className=" bg-cstmgreen  pl-1 rounded-md"
-            key={idx}
-          >
-            <div className="card bg-cstmblack p-4 flex flex-col gap-4 text-slate-200 ">
-              <div className="">
-                <h3 className="text-lg font-semibold">
-                  <span className="text-cstmgreen">{position}</span> @{at}
-                </h3>
-                <p className="text-xs -mt-1">{date}</p>
-              </div>
-              <div>{description}</div>
-              <div className="flex flex-wrap flex-row gap-2">
-                {tech.map((name, i) => (
-                  <TechIcon name={name} key={i} />
-                ))}
-              </div>
+          <div className="-mx-4 group p-4 pb-6 flex flex-col gap-4 hover:bg-gray-400 rounded-md bg-clip-padding hover:backdrop-filter hover:backdrop-blur-sm hover:bg-opacity-10 transition duration-200 ease-in-out">
+            <div className="">
+              <Link href={link} className="text-lg font-semibold cursor-pointer">
+                <span className="group-hover:text-cstmgreen text-cstmgreen lg:text-slate-200">
+                  {position}{" "}
+                </span>
+                · {at}
+              </Link>
+              <p className="text-xs">{date}</p>
             </div>
-          </Link>
+            <div className="text-sm flex flex-col gap-2">
+              {description.map((desc) => (
+                <span>{desc}</span>
+              ))}
+            </div>
+            <div className="flex flex-wrap flex-row gap-2">
+              {tech.map((item) => (
+                <TechIcon name={item} />
+              ))}
+            </div>
+          </div>
         ))}
       </div>
     </div>
