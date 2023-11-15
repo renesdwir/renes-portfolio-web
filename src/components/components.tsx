ba513b03283cb5bbd86400067d049032840d5f20
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-11-15 21:40:20 +0700

    register components

---
+++ src/components/components.tsx
@@ -0,0 +1,7 @@
+export { default as About } from "./about.component";
+export { default as Education } from "./education.component";
+export { default as Experience } from "./experience.component";
+export { default as Footer } from "./footer.component";
+export { default as Profile } from "./profile.component";
+export { default as Project } from "./project.component";
+export { default as Tech } from "./tech.component";
