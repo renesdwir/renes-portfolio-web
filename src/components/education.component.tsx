c0396f99ae21d8f30c3245d8615af64e8f51e967
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-11-15 21:39:49 +0700

    refactor education

--- src/components/education.component.tsx
+++ src/components/education.component.tsx
@@ -1,29 +1,31 @@
-import { EducationDataInterface } from "@/data/data-types";
-import Hacktiv from "../../public/icons/hacktivsvg.svg";
 import Title from "./title.component";
 import Image from "next/image";
-interface EducationPropsTypes {
-  data: EducationDataInterface[];
-}
-const Education = (props: EducationPropsTypes) => {
-  const { data: datas } = props;
+
+import { education_data } from "@/data/data";
+import Link from "next/link";
+
+const Education = () => {
+  const datas = education_data;
   return (
     <div id="education" className="pt-16 mb-16 lg:pt-0 lg:mb-36">
       <Title text="Education" />
-      <div className="flex flex-col gap-5">
-        {datas.map(({ achievement, date, desc, logo, name }, idx) => (
-          <div key={idx}>
-            <Image
-              src={`/icons/${logo}`}
-              alt={name}
-              width={0}
-              height={0}
-              className="w-36"
-            />
-            <h3 className="text-lg font-semibold text-cstmgreen">{name}</h3>
-            <p className="text-xs -mt-1 mb-3">{date}</p>
-            <p>{achievement}</p>
-            <p>{desc}</p>
+      <div className="flex flex-col">
+        {datas.map((data) => (
+          <div className="flex flex-row items-start gap-4">
+            <div>
+              <Image src={data.imgUrl} alt="Hacktiv8 Indonesia" width={70} height={70} />
+            </div>
+            <div className="flex-1">
+              <Link href={data.url} className="font-bold text-cstmgreen">
+                {data.name}
+              </Link>
+              <p className="text-sm">{data.subTitle}</p>
+              <p className="text-sm opacity-70">{data.date}</p>
+              <p
+                className="text-sm whitespace-pre-line mt-1"
+                dangerouslySetInnerHTML={{ __html: data.desc }}
+              ></p>
+            </div>
           </div>
         ))}
       </div>
