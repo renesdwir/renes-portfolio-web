6a7086052a524cd656c8b38e612cb8fd23c9d17f
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-11-15 21:40:05 +0700

    change style tech component

--- src/components/techIcon.component.tsx
+++ src/components/techIcon.component.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TechIcon = ({ name }: { name: string }) => {
   return (
-    <div className="px-3 py-1 bg-cstmgreen/10 text-cstmgreen text-center text-xs rounded-md">
+    <div className="px-3 py-1.5 bg-cstmgreen/10 font-light font-mono text-cstmgreen text-center text-xs">
       {name}
     </div>
   );
