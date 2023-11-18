e7cd01318249e39be23fbfe2fabb53ffe3a3dad3
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-11-18 23:17:48 +0700

    update project data

--- src/components/project.component.tsx
+++ src/components/project.component.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Title from "./title.component";
 import TechIcon from "./techIcon.component";
 import { project_data } from "@/data/data";
+import Link from "next/link";
 
 const Project = () => {
   const datas = project_data;
@@ -15,10 +16,14 @@ const Project = () => {
             key={idx}
             className="-mx-4 group p-4 pb-6 gap-x-4 gap-y-4 flex flex-col-reverse lg:flex-row  hover:bg-gray-400 rounded-md bg-clip-padding hover:backdrop-filter hover:backdrop-blur-sm hover:bg-opacity-10 transition duration-200 ease-in-out"
           >
-            <div className="w-52 h-28  relative group-hover:outline group-hover:outline-2 outline-gray-400/20 rounded">
+            <div
+              className={`w-52 h-28  relative group-hover:outline group-hover:outline-2 outline-gray-400/20 rounded ${
+                img !== "" ? "" : "hidden lg:block"
+              }`}
+            >
               <Image
                 alt={title + " Photo"}
-                src={img}
+                src={img !== "" ? img : "/noimage.jpg"}
                 fill={true}
                 sizes="(max-width:768px) 100vw, 700px"
                 className="object-cover rounded text-xs"
@@ -26,12 +31,16 @@ const Project = () => {
             </div>
             <div className="flex-1 flex flex-col gap-y-2">
               <div>
-                <div className="font-bold relative inline-block cursor-pointer group-hover:text-cstmgreen">
+                <Link
+                  href={url}
+                  target="_blank"
+                  className="font-bold relative inline-block cursor-pointer group-hover:text-cstmgreen"
+                >
                   {title}
                   <BiArrowBack className="text-slate-200 absolute -right-5 top-[6.2px] rotate-[135deg] group-hover:translate-x-1 group-hover:-translate-y-1 ease-in duration-100 text-sm" />
-                </div>
+                </Link>
               </div>
-              <p className="text-sm">{desc}</p>
+              <p className="text-sm font-light whitespace-pre-line">{desc}</p>
               <div className="flex flex-wrap flex-row gap-2">
                 {tech.map((name, idx) => (
                   <TechIcon name={name} key={idx} />
@@ -40,6 +49,24 @@ const Project = () => {
             </div>
           </div>
         ))}
+      </div>
+      <div className="text-center mt-3 ">
+        <div className=" w-full h-32 relative mb-4">
+          <Image
+            src="https://streak-stats.demolab.com?user=renesdwir&locale=en&mode=daily&theme=dark&hide_border=false&border_radius=5&order=3"
+            alt="streak graph"
+            fill={true}
+            className="object-contain"
+          />
+        </div>
+        <Link
+          className="text-sm underline-offset-4 underline hover:text-cstmgreen relative group"
+          href={"https://github.com/renesdwir?tab=repositories"}
+          target="_blank"
+        >
+          See all of my repositories on GitHub
+          <BiArrowBack className="group:text-cstmgreen absolute -right-[17px] top-1 rotate-[180deg] group-hover:translate-x-1  ease-in duration-100 text-sm" />
+        </Link>
       </div>
     </div>
   );
