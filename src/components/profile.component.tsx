84a6e85e2b57ec03ef7c7383fc5e4b1f5d596853
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-11-20 18:49:00 +0700

    set location when click

--- src/components/profile.component.tsx
+++ src/components/profile.component.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "./navigation.component";
 import Link from "next/link";
-
 import { profile_data, navigation_data, name } from "@/data/data";
+const getHash = () =>
+  typeof window !== "undefined" ? decodeURIComponent(window.location.hash) : "";
+
 const Profile = () => {
   const { position, profileDescription } = profile_data;
   const { contacts, nav } = navigation_data;
+  const [location, setLocation] = useState("");
+  const handleClick = (loc: string) => {
+    setLocation(`#${loc}`);
+  };
+  useEffect(() => {
+    const hash = getHash();
+    setLocation(hash !== undefined ? hash : "");
+  }, []);
+  console.log(location);
   return (
     <div className="lg:sticky lg:top-0 lg:max-h-screen lg:w-1/2 lg:py-24">
       <h1 className="inline-block font-extrabold text-4xl md:text-4xl lg:text-5xl tracking-tighter selection:bg-cstmgreen selection:text-cstmblack">
@@ -26,7 +37,12 @@ const Profile = () => {
       </div>
       <div className="flex-col my-6 hidden lg:flex gap-2">
         {nav.map((text, idx) => (
-          <Nav to={text} key={idx} />
+          <Nav
+            to={text}
+            key={idx}
+            handleClick={() => handleClick(text)}
+            isActive={location === `#${text}`}
+          />
         ))}
       </div>
       <div className="flex flex-row gap-5 mt-8">
