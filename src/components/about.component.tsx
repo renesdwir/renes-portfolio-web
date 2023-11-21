b01bf4c2e415bcddbe7bba9a6ccce138f193de8b
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-11-21 21:38:56 +0700

    add section id

--- src/components/about.component.tsx
+++ src/components/about.component.tsx
@@ -4,14 +4,14 @@ import { about_data } from "@/data/data";
 const About = () => {
   const datas = about_data;
   return (
-    <div id="about" className="pt-16  lg:pt-0 lg:mb-36">
+    <section id="about" className="pt-16  lg:pt-0 lg:mb-36">
       <Title text="About" />
       <div className="space-y-4">
         {datas.map((data, idx) => (
           <p key={idx}>{data}</p>
         ))}
       </div>
-    </div>
+    </section>
   );
 };
 
