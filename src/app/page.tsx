33cee5a5c76fbf948676e640822915117e82c7a4
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-11-23 21:13:58 +0700

    fix padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
     <main className="font-poppins mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0 ">
       <div className="lg:flex lg:justify-between lg:gap-4">
         <Profile />
-        <div className="lg:w-1/2 lg:py-24 mt-10 lg:mt-0">
+        <div className="lg:w-1/2 lg:pb-24 mt-10 lg:mt-0 ">
           <About />
           <Experience />
           <Tech />
