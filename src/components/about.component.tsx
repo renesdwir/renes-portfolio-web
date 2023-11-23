09e1bd3f306669ca4aa887911c7d7aa69e6919c8
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-11-23 21:14:14 +0700

    fix padding

--- src/components/about.component.tsx
+++ src/components/about.component.tsx
@@ -4,7 +4,7 @@ import { about_data } from "@/data/data";
 const About = () => {
   const datas = about_data;
   return (
-    <section id="about" className="pt-16  lg:pt-0 lg:mb-36">
+    <section id="about" className="pt-16  lg:pt-24 ">
       <Title text="About" />
       <div className="space-y-4">
         {datas.map((data, idx) => (
