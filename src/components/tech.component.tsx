862f667ad685b001fce8c49ff7f68c132cb07020
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-12-02 22:40:18 +0700

    improve accessibility lighthouse

--- src/components/tech.component.tsx
+++ src/components/tech.component.tsx
@@ -8,9 +8,9 @@ const Tech = () => {
       <Title text="Some Technologies I've worked with" />
       <div className="flex flex-col gap-8">
         <div>
-          <h4 className="pl-2 border-l-2 border-cstmgreen text-sm mb-3">
+          <h1 className="pl-2 border-l-2 border-cstmgreen text-sm mb-3">
             Back-End Development
-          </h4>
+          </h1>
           <div className="grid grid-cols-3 lg:grid-cols-4 gap-x-1 lg:gap-x-3 gap-y-1 lg:gap-y-1.5">
             {datas.backend.map((data, idx) => (
               <div key={idx} className="flex flex-row items-center gap-2">
@@ -21,9 +21,9 @@ const Tech = () => {
           </div>
         </div>
         <div>
-          <h4 className="pl-2 border-l-2 border-cstmgreen text-sm mb-3">
+          <h1 className="pl-2 border-l-2 border-cstmgreen text-sm mb-3">
             Front-End Development
-          </h4>
+          </h1>
           <div className="grid grid-cols-3 lg:grid-cols-4 gap-x-1 lg:gap-x-3 gap-y-1 lg:gap-y-1.5">
             {datas.frontend.map((data, idx) => (
               <div key={idx} className="flex flex-row items-center gap-2">
@@ -34,7 +34,7 @@ const Tech = () => {
           </div>
         </div>
         <div>
-          <h4 className="pl-2 border-l-2 border-cstmgreen text-sm mb-3">I've used it</h4>
+          <h1 className="pl-2 border-l-2 border-cstmgreen text-sm mb-3">I've used it</h1>
           <div className="grid grid-cols-3 lg:grid-cols-4 gap-x-1 lg:gap-x-3 gap-y-1 lg:gap-y-1.5">
             {datas.extra.map((data, idx) => (
               <div key={idx} className="flex flex-row items-center gap-2">
